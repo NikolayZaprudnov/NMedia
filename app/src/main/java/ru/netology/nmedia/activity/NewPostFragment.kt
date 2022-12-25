059ea3b62362165115b364658a87b335f0812925
Nikola Zaprudnov <kolya-zapru@yandex.ru>
2022-12-25 20:43:33 +0300

    draft

--- app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -1,5 +1,6 @@
 package ru.netology.nmedia.activity
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,44 +18,47 @@ class NewPostFragment : Fragment() {
     companion object {
         var Bundle.textArg: String? by StringArg
     }
+
     private val viewModel: PostViewModel by viewModels(
         ownerProducer = ::requireParentFragment
     )
+    val SAVE_DRAFT_FILENAME = "Draft"
+    val SETTING = "settings"
+    var savedDraft: SharedPreferences? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         val binding = FragmentNewPostBinding.inflate(
             inflater,
             container,
             false
         )
-        var draft:String? = arguments?.textArg.toString()
-        val callback = requireActivity().onBackPressedDispatcher.
-            addCallback(viewLifecycleOwner){
-                draft = binding.editText.text.toString()
-                findNavController().navigate(R.id.feedFragment,
-                    Bundle().apply {
-                        textArg = draft
-                    }
-                )
-            }
-//        super.onCreate(savedInstanceState)
-//        val binding = ActivityNewPostBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-        if (draft != null){
-            binding.editText.setText(draft)
+        var draft: String?
+        savedDraft = context?.getSharedPreferences(SAVE_DRAFT_FILENAME, 0)
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            draft = binding.editText.text.toString()
+            val editor = savedDraft!!.edit()
+            editor.putString(SETTING, draft)
+            editor.apply()
+            findNavController().navigate(R.id.feedFragment)
         }
+        draft = savedDraft!!.getString(SETTING, "").toString()
+        binding.editText.setText(draft)
         binding.editText.requestFocus()
         binding.ok.setOnClickListener {
             viewModel.changeContent(binding.editText.text.toString())
             viewModel.save()
-            draft = null
+            val editor = savedDraft!!.edit()
+            draft = ""
+            editor.putString(SETTING, draft)
+            editor.apply()
             findNavController().navigateUp()
         }
         return binding.root
-    }}
+    }
+}
 //            val intent = Intent()
 //            if( binding.editText.text.isBlank()){
 //                setResult(Activity.RESULT_CANCELED, intent)
