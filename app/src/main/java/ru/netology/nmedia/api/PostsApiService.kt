1116c13bd674934c5c61b47290872ea8b8eea780
Nikola Zaprudnov <kolya-zapru@yandex.ru>
2023-02-06 19:37:04 +0300

    retrofit ver.2

--- app/src/main/java/ru/netology/nmedia/api/PostsApiService.kt
+++ app/src/main/java/ru/netology/nmedia/api/PostsApiService.kt
@@ -5,9 +5,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 import retrofit2.http.*
+import ru.netology.nmedia.BuildConfig
 import ru.netology.nmedia.dto.Post
 
-private const val BASE_URL = "http://10.0.2.2:9999/api/slow/"
+private const val BASE_URL = "${BuildConfig.BASE_URL}/api/slow/"
 
 interface PostsApiService {
     @GET("posts")
