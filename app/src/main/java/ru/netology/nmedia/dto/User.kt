143393acc4192cde9fcc2678df4fb32bd6a9745c
Nikola Zaprudnov <kolya-zapru@yandex.ru>
2023-09-14 16:34:18 +0300

    Diplom

--- app/src/main/java/ru/netology/nmedia/dto/User.kt
+++
@@ -1,6 +0,0 @@
-package ru.netology.nmedia.dto
-
-data class User(
-    val id: Long,
-    val token: String,
-)
--- app/src/main/java/util/AndroidUtils.kt
+++ app/src/main/java/util/AndroidUtils.kt
@@ -1,4 +1,4 @@
-package ru.netology.nmedia.util
+package ru.netology.nikjob.util
 
 import android.content.Context
 import android.view.View
